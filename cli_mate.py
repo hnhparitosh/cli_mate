64254ec32986583829492089d73b4d9df924eeb4
Paritosh Dahiya <dahiyaparitosh@gmail.com>
2023-06-26 20:35:30 +0530

    ✅coordinates in CLI + complete GUI

--- cli_mate.py
+++ cli_mate.py
@@ -1,7 +1,5 @@
 import requests
-import json
 import argparse
-import sys
 import dotenv
 import os
 
@@ -18,34 +16,62 @@ def get_weather(city):
     Returns: dict: The weather data.
     
     '''
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
         return data
     except Exception as err:
-        print(f'Error occured while fetching the weather: {err}')
-        sys.exit(1)
+        print(f'Error occured while fetching the weather for: {city}')
+        return None
+
+# write a function that take coordinates of a place and returns the current weather for that place
+def get_weather_by_coords(lat, lon):
+    '''
+    Get the current weather for a place using coordinates.
+    Args: lat (float): The latitude of the place.
+          lon (float): The longitude of the place.
+    Returns: dict: The weather data.
+    
+    '''
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except Exception as err:
+        print(f'Error occured while fetching the weather for: {lat}, {lon}')
+        return None
 
 # print the data in a readable format
 def print_weather(data):
-    print(data)
-    print(f'Current weather in {data["name"]}:')
-    print(f'{data["weather"][0]["main"]} - {data["weather"][0]["description"]}')
-    print(f'Temperature: {data["main"]["temp"]}')
-    print(f'Feels like: {data["main"]["feels_like"]}')
-    print(f'Humidity: {data["main"]["humidity"]}')
-    print(f'Wind speed: {data["wind"]["speed"]}')
+    print('📍  Location:\t\t' + data['name'])
+    print('🌤️  Current Weather:\t'+ data['weather'][0]['main'])
+    print('🌡️  Temperature:\t' + str(data['main']['temp'])+'°C')
+    print('💧  Humidity:\t\t' + str(data['main']['humidity'])+' %')
+    print('💨  Wind speed:\t\t' + str(data['wind']['speed'])+' m/s')
+    print('🧊 Pressure:\t\t' + str(data['main']['pressure'])+' hPa')
+    
 
 # write main function to take in command line arguments using argparse
+# user can either enter a city name or coordinates
 def main():
-    parser = argparse.ArgumentParser(description='Get the current weather for a city.')
-    parser.add_argument('city', help='The city to get the weather for.')
+    parser = argparse.ArgumentParser(description='Get the current weather for a city or place.')
+    parser.add_argument('-c', '--city', help='The city to get the weather for.')
+    parser.add_argument('-lat', '--latitude', help='The latitude of the place.')
+    parser.add_argument('-lon', '--longitude', help='The longitude of the place.')
     args = parser.parse_args()
-    city = args.city
-    data = get_weather(city)
-    print_weather(data)
+    if args.city:
+        data = get_weather(args.city)
+        print_weather(data)
+    elif args.latitude and args.longitude:
+        data = get_weather_by_coords(args.latitude, args.longitude)
+        print_weather(data)
+    else:
+        print('Please enter a city name or coordinates.')
+
 
 if __name__ == '__main__':
     main()
--- gui_mate.py
+++ gui_mate.py
@@ -11,7 +11,7 @@ class App(ctk.CTk):
         super().__init__(*args, **kwargs)
 
         self.title('GUI-Mate for CLI-Mate')
-        self.geometry('800x600')
+        self.geometry('900x700')
         self.minsize(800, 600)
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -20,10 +20,14 @@ class App(ctk.CTk):
 
         self.marker = None
         self.geolocator = Nominatim(user_agent='geoapiExercises')
+
+        # set default font
+        self.default_font = ctk.CTkFont(family='Robot', size=20)
+        self.big_font = ctk.CTkFont(family='Robot', size=24)
     
         # create two frames in the window
-        self.grid_columnconfigure(0, weight=0)
-        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=5)
         self.grid_rowconfigure(0, weight=1)
 
         self.left_frame = ctk.CTkFrame(master=self,width=200, corner_radius=0, fg_color=None)
@@ -33,45 +37,91 @@ class App(ctk.CTk):
         self.right_frame.grid(row=0, column=1,rowspan=1, padx=0, pady=0, sticky='nsew')
 
         # left frame
+        self.left_frame.grid_columnconfigure(0, weight=1)
+        # self.right_frame.grid_columnconfigure(1, weight=1)
+        self.left_frame.grid_rowconfigure(0, weight=1)
+        self.left_frame.grid_rowconfigure(1, weight=1)
+        self.left_frame.grid_rowconfigure(2, weight=1)
+        self.left_frame.grid_rowconfigure(3, weight=1)
+        self.left_frame.grid_rowconfigure(4, weight=1)
 
-        # create label for displaying the city name and weather details from cli_mate
-        self.city_label = ctk.CTkLabel(master=self.left_frame, text='City Name', anchor='w')
-        self.city_label.grid(row=0, column=0, padx=(20, 20), pady=(20, 0))
+        # create a label for current weather, temperature, wind speed, humidity and pressure
+        self.current_weather_label = ctk.CTkLabel(master=self.left_frame, text='Current Weather:\n🌤️', anchor='w',font=self.default_font)
+        self.current_weather_label.grid(row=0, column=0, padx=(20, 20), pady=(20, 0))
 
-        self.weather_label = ctk.CTkLabel(master=self.left_frame, text='Weather Details', anchor='w')
-        self.weather_label.grid(row=1, column=0, padx=(20, 20), pady=(20, 0))
+        self.temperature_label = ctk.CTkLabel(master=self.left_frame, text='Temperature:\n🌡️', anchor='w',font=self.default_font)
+        self.temperature_label.grid(row=1, column=0, padx=(20, 20), pady=(20, 0))
+
+        self.wind_speed_label = ctk.CTkLabel(master=self.left_frame, text='Wind Speed:\n💨', anchor='w',font=self.default_font)
+        self.wind_speed_label.grid(row=2, column=0, padx=(20, 20), pady=(20, 0))
+
+        self.humidity_label = ctk.CTkLabel(master=self.left_frame, text='Humidity:\n💧', anchor='w',font=self.default_font)
+        self.humidity_label.grid(row=3, column=0, padx=(20, 20), pady=(20, 0))
+
+        self.pressure_label = ctk.CTkLabel(master=self.left_frame, text='Pressure:\n🧊', anchor='w',font=self.default_font)
+        self.pressure_label.grid(row=4, column=0, padx=(20, 20), pady=(20, 20))
 
         # right frame
+        self.right_frame.grid_columnconfigure(0, weight=1)
+        # self.right_frame.grid_columnconfigure(1, weight=1)
+        self.right_frame.grid_rowconfigure(0, weight=0)
+        self.right_frame.grid_rowconfigure(1, weight=1)
+
+        # create a label for the city name
+        self.city_name_label = ctk.CTkLabel(master=self.right_frame, text='📍 New Delhi, India', anchor='w',font=self.big_font)
+        self.city_name_label.grid(row=0, column=0, padx=20, pady = 20)
+
 
         #  create map view
         self.map_view = tkmap.TkinterMapView(master=self.right_frame, corner_radius=0)
-        self.map_view.grid(row=0, column=0, padx=(0, 0), pady=(0, 0), sticky='nsew')
-        self.marker = self.map_view.set_marker(28.644800, 77.216721)
-
+        self.map_view.grid(row=1, column=0,columnspan=2, padx=(0, 0), pady=(0, 0), sticky='nsew')
+        
         self.map_view.add_left_click_map_command(self.add_marker_event)
 
         # set default values of map widget
         self.map_view.set_address("New Delhi")
         self.map_view.set_zoom(5)
+        self.marker = self.map_view.set_marker(28.644800, 77.216721)
+        self.get_weather_details((28.644800, 77.216721))
         self.map_view.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+
+    # function to get weather details of the city and display it in the left frame
+    def get_weather_details(self, coords):
+        weather_data = cli_mate.get_weather_by_coords(coords[0], coords[1])
+        
+        if weather_data:
+            self.current_weather_label.configure(text='Current Weather:\n🌤️ '+str(weather_data['weather'][0]['main']))
+            self.temperature_label.configure(text='Temperature:\n🌡️ '+str(weather_data['main']['temp'])+'°C')
+            self.wind_speed_label.configure(text='Wind Speed:\n💨 '+str(weather_data['wind']['speed'])+' m/s')
+            self.humidity_label.configure(text='Humidity:\n💧 '+str(weather_data['main']['humidity'])+'%')
+            self.pressure_label.configure(text='Pressure:\n🧊 '+str(weather_data['main']['pressure'])+' hPa')
+            # type emoji for Atmospheric pressure
+
+        else:
+            self.current_weather_label.configure(text='Current Weather:\n🌤️ --')
+            self.temperature_label.configure(text='Temperature:\n🌡️ -- °C')
+            self.wind_speed_label.configure(text='Wind Speed:\n💨 -- m/s')
+            self.humidity_label.configure(text='Humidity:💧\n -- %')
+            self.pressure_label.configure(text='Pressure:\n🧊 -- hPa')
 
 
     def add_marker_event(self, coords):
         self.marker.delete()
         self.marker = self.map_view.set_marker(coords[0], coords[1])
-        location = self.geolocator.geocode(f'{coords[0]},{coords[1]}')
-        location = self.geolocator.reverse(f'{coords[0]},{coords[1]}')
-        print(location.raw)
+        location_data = self.geolocator.reverse(f'{coords[0]},{coords[1]}', language='en')
+        
+        #  get only state name from the location data
+        try:
+            location = location_data.raw['address']['state_district']+','+location_data.raw['address']['state']+','+location_data.raw['address']['country']
+        except:
+            location = location_data.raw['address']['state']+','+location_data.raw['address']['country']
+    
+        self.city_name_label.configure(text='📍 '+location)
+        self.get_weather_details(coords)
+        # print(location.raw)
 
     def on_closing(self, event=0):
         self.destroy()
-
-    
-        
-
-
-
-
 
     def start(self):
         self.mainloop()
